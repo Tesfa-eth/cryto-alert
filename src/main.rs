74f3aa89ae7cae915ef599cafb0a94fcd9284a06
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-05-23 08:20:30 -0400

    added tests

--- src/main.rs
+++ src/main.rs
@@ -141,3 +141,31 @@ fn main() {
     // Block on the price monitoring task.
     rt.block_on(monitor_price_change(&url, interval)).unwrap();
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio_test::block_on;
+
+    #[test]
+    fn test_validate_u64_input() {
+        assert!(validate_u64_input("5").is_ok());
+        assert!(validate_u64_input("five").is_err());
+    }
+
+    #[test]
+    fn test_parse_price() {
+        let response_data = r#"{
+            "price": "123.45"
+        }"#;
+        assert_eq!(parse_price(response_data).unwrap(), "123.45");
+    }
+
+    #[test]
+    fn test_send_get_request() {
+        let fake_url = "https://httpbin.org/get";
+        let response = block_on(send_get_request(fake_url));
+        assert!(response.is_ok());
+    }
+}
