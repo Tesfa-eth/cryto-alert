b97c68c027b17e7acacf3eafc98abc393dcf0168
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2023-05-23 08:49:49 -0400

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -52,33 +52,48 @@ fn validate_u64_input(input: &str) -> Result<u64, Box<dyn Error>> {
 }
 
 async fn monitor_price_change(sell_token: &str, buy_token: &str, interval: u64) -> Result<(), Box<dyn Error>> {
+    println!("Monitoring price change for {} -> {}", sell_token, buy_token);
     // Create the URL for the 0x API.
     let url = format!("https://api.0x.org/swap/v1/quote?sellAmount=100000000&buyToken={}&sellToken={}", sell_token, buy_token);
-
+    
+    println!("Checking initial price...");
     // Get the current price.
-    let mut current_price = get_price(&url).await?;
+    let mut current_price = match get_price(&url).await {
+        Ok(price) => price,
+        Err(e) => {
+            println!("Failed to fetch initial price: {:?}", e);
+            return Ok(());  // Return early.
+        }
+    };
 
-    println!("Monitoring price change for {} -> {}", sell_token, buy_token);
+    println!("Current price: {}", current_price);
 
     loop {
         println!("Checking price...");
-        // Get the new price.
-        let new_price = get_price(&url).await?;
-
-        println!("Current price: {}", current_price);
-
-        // Compare the new price with the current price.
-        if new_price != current_price {
-            println!("Price changed: {} -> {}", current_price, new_price);
-            current_price = new_price;
-        }
-
-        println!("Price unchanged. Checking again in {} seconds...", interval);
-
         // Wait for the specified interval.
         sleep(Duration::from_secs(interval)).await;
+
+        // Get the new price.
+        match get_price(&url).await {
+            Ok(new_price) => {
+                // Compare the new price with the current price.
+                if new_price != current_price {
+                    println!("Price changed: {} -> {}", current_price, new_price);
+                    current_price = new_price;
+                }
+            }
+            Err(e) => {
+                println!("Failed to fetch price: {:?}", e);
+                continue;  // Skip this iteration.
+            }
+        };
+
+        println!("Price unchanged. Checking again in {} seconds...", interval);
     }
+
+    
 }
+
 
 async fn get_price(url: &str) -> Result<String, Box<dyn Error>> {
     // Send a GET request to the 0x API and get the response data.
